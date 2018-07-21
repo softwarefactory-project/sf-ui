50a36fc0424d096afd35e737b7e7530a25413964
Tristan Cacqueray <tdecacqu@redhat.com>
2018-07-21 04:49:31 +0000

    Add gerrit iframe
    Todo:
    * navigation back and from review from the ui doesn't work,
      maybe route has to reset location url to /ui
    * remove console.log
    
    Change-Id: I284f870471cf2a4c43b655e56eb114c18f99941f

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -1,16 +1,58 @@
 import * as React from 'react';
 import { Grid } from 'patternfly-react';
+import Iframe from 'react-iframe';
+import ReactDOM from 'react-dom';
 
-const ReviewPage = () => (
-  <Grid fluid className="container-pf-nav-pf-vertical">
-    <Grid.Row>
-      <Grid.Col xs={12}>
-        <div className="page-header">
-          <h1>Review Page</h1>
-        </div>
-      </Grid.Col>
-    </Grid.Row>
-  </Grid>
-);
+/* Add this to GerritSiteHeader.html
+    if (window.parent === window) {
+      console.log("Setting location to " + window.location.href.replace('/r/', '/ui/#/review/'));
+      window.location.href = window.location.href.replace('/r/', '/ui/#/review/');
+    }
+*/
+
+class ReviewPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+    this.frameLoaded = this.frameLoaded.bind(this);
+    this.frameHashChanged = this.frameHashChanged.bind(this);
+    this.idx = 0;
+  }
+  frameLoaded(evt) {
+    console.log("frameLoaded");
+    const iframe = ReactDOM.findDOMNode(this.myRef.current);
+    iframe.contentWindow.window.addEventListener(
+      "hashchange", this.frameHashChanged);
+    this.updateUrl(this.gerritPath);
+  }
+  updateUrl(newUrl) {
+    console.log("Pusing new url [" + newUrl + "]");
+    window.history.pushState("Test" + this.idx, "SF", newUrl);
+    this.idx += 1;
+  }
+  frameHashChanged(evt) {
+    console.log(evt);
+    this.updateUrl(evt.newURL);
+  }
+  render() {
+    this.gerritPath = "/r/" + this.props.location.hash;
+    console.log("Loading gerrit ui to [" + this.gerritPath + "]");
+    //this.updateUrl(gerritPath);
+    return (
+      <Grid fluid className="container-pf-nav-pf-vertical">
+        <Grid.Row>
+          <Iframe
+            ref={this.myRef}
+            url={this.gerritPath}
+            width="90%"
+            height="90%"
+            display="initial"
+            onLoad={this.frameLoaded}
+            />
+        </Grid.Row>
+      </Grid>
+    );
+  }
+}
 
 export default ReviewPage;
